a72ea01fddabdb6cbbdf454fb56f5021ffe671db
Stewart ilondanga <stilondanga@gmail.com>
2017-11-26 13:59:30 +0300

    add src/app/app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Http, Response} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'User Lookup';
+
+  constructor(private http:Http){}
+  userName="";
+  githubData:any="";
+
+
+userLookup(){
+  this.http.get("https://api.github.com/users/"+this.userName).subscribe((response:Response)=>{
+    const userData=response.json();
+    this.githubData=userData;
+    console.log(userData);
+  }
+}
+)
 }
